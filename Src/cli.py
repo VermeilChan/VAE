3e2fc30b73060d28ed7eb7fa6b427cc23fd52a02
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-08-29 15:44:58 +0100

    Bump Version

--- Src/cli.py
+++ Src/cli.py
@@ -3,7 +3,7 @@ from platform import system, architecture
 from extract_addons import main as extract_addons
 from extract_archives import main as extract_archives
 
-version = "v2.0.3"
+version = "v2.0.4"
 
 def display_menu():
     print(
